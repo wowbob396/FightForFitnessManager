4f849be5889f1d4186d6251de235400ed74dbf34
wowbob396 <wowbob396@gmail.com>
2019-05-08 20:27:58 -0500

    added react router dom

--- src/components/Login/src/Login.tsx
+++ src/components/Login/src/Login.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import createStyles from '@material-ui/core/styles/createStyles';
 import './Login.scss';
-export default class Login extends React.Component {
+
+const styles = createStyles({
+    main: {
+        width: 'auto',
+        display: 'block',
+
+    },
+    paper: {
+        marginTop: 50,
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 50,
+    }
+});
+
+export default class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login Page</h1>
-            </div>
+            <main>
+                <CssBaseline/>
+                <Paper>
+
+                </Paper>
+            </main>
         );
     }
 }
