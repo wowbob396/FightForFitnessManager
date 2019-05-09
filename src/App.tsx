27be39462118c92cbabbaf922ca21f6012743256
wowbob396 <wowbob396@gmail.com>
2019-05-08 20:36:10 -0500

    added typescript definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Login from './components/Login';
+import { Redirect, Route, Switch } from 'react-router';
+import { Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
 import './App.scss';
 
 const App: React.FC = () => {
