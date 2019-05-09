8bcf9ddca4c14db2886ec4f3e12020bf90c2a16a
wowbob396 <wowbob396@gmail.com>
2019-05-08 19:40:10 -0500

    added login component

---
+++ src/components/Login/src/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.scss';
+export default class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Login Page</h1>
+            </div>
+        );
+    }
+}
