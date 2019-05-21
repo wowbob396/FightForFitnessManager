b2b046e6c37fb80d12f892932b557e6019620ca9
wowbob396 <wowbob396@gmail.com>
2019-05-21 17:51:47 -0500

    started working on firebase authentication

---
+++ src/components/Firebase/firebase.ts
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import React from 'react';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmTq4-KRemuMEDTDCpTy6WKJzq-D8867A",
+    authDomain: "fightforfitnessflutter.firebaseapp.com",
+    databaseURL: "https://fightforfitnessflutter.firebaseio.com",
+    projectId: "fightforfitnessflutter",
+    storageBucket: "fightforfitnessflutter.appspot.com",
+    messagingSenderId: "1008445354039",
+    appId: "1:1008445354039:web:80d1bddacf7e32b3"
+  };
+
+  class Firebase {
+      private auth;
+      constructor() {
+          app.initializeApp(firebaseConfig);
+          this.auth = app.auth();
+      }
+
+      public doCreateUserWithEmailAndPassword = (email, password) => {
+          this.auth.createUserWithEmailAndPassword(email,password);
+      }
+
+      public doSignInWithEmailAndPassword = (email, password) => {
+          this.auth.signInWithEmailAndPassword(email,password);
+      }
+
+      public doSignOut = () => this.auth.signOut();
+  }
+
+const FirebaseContext = React.createContext(null);  
+
+export default Firebase;
+
+export { FirebaseContext }; 
