e293fac63b4129a603e31b260983cfc0a56b7f46
wowbob396 <wowbob396@gmail.com>
2019-05-24 16:12:37 -0500

    reformatted libraries

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase/firebase';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 
 ReactDOM.render(
